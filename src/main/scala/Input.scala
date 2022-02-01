61c46ae5386cf6962c1b9ca16fe47d15674c2234
mateusz.dolata <mateusz.dolata@ocado.com>
2022-02-01 22:17:18 +0100

    day1 part 1

---
+++ src/main/scala/Input.scala
@@ -0,0 +1,5 @@
+object Input {
+  val test = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
+  val result = 5
+
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,21 @@
+import scala.annotation.tailrec
+
 object Main {
+
+  def solution(input: Array[Int]): Int = {
+    @tailrec
+    def sol(current: Int, input: Array[Int], counter: Int): Int =
+      if (input.isEmpty) counter
+      else if (current > input.head) sol(input.head, input.tail, counter)
+      else sol(input.head, input.tail, counter + 1)
+
+    sol(input.head, input.tail, 0)
+  }
+
+
   def main(args: Array[String]): Unit = {
-    println("Hello AOC 2021!")
+    val input = Input.test.split("\n").map(s => s.toInt)
+    println(solution(input) == Input.result)
+    println(solution(input))
   }
 }
