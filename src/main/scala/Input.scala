11c0c36f243fcb020598ca35b07e9183abd7eca1
mateusz.dolata <mateusz.dolata@ocado.com>
2022-02-01 22:51:17 +0100

    day1 part 2

--- src/main/scala/Input.scala
+++ src/main/scala/Input.scala
@@ -1,5 +1,5 @@
 object Input {
   val test = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
-  val result = 5
+  val result = 6
 
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,23 @@
+
 import scala.annotation.tailrec
 
+case class Triplet(lst: Array[Int])
+
 object Main {
+
+  def triple(input: Array[Int], triplets: Array[Triplet] = Array()): Array[Triplet] =
+    if (input.take(3).length < 3) triplets
+    else triple(input.tail, triplets.appended(Triplet(input.take(3))))
 
   def solution(input: Array[Int]): Int = {
     @tailrec
     def sol(current: Int, input: Array[Int], counter: Int): Int =
       if (input.isEmpty) counter
-      else if (current > input.head) sol(input.head, input.tail, counter)
+      else if (current >= input.head) sol(input.head, input.tail, counter)
       else sol(input.head, input.tail, counter + 1)
 
-    sol(input.head, input.tail, 0)
+    val triplets = triple(input).map(l => l.lst.sum)
+    sol(triplets.head, triplets.tail, 0)
   }
 
 
